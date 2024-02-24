23461a083d1946c6176a7244ea00204a45a65e58
octuaOSdev <144250548+octuaOSdev@users.noreply.github.com>
2024-02-24 23:24:49 +0100

    Update main window.py

--- main window.py
+++ main window.py
@@ -1,24 +1,3 @@
-import tkinter as tk
-tempclick = 0
-def main():
-    root = tk.Tk()
-    root.title("untitled game")
-    root.geometry("300x200")  # Set window dimensions (width x height)
+import pygame
 
-    label = tk.Label(root, text="Hello, world!")
-    label.pack(pady=20)  # Add some padding
-
-    button = tk.Button(root, text="click me", command=lambda: buttonclick())
-    button.pack()
-
-    root.mainloop()
-def buttonclick():
-    clicks = 0
-    clicks += 1
-    print (clicks)
-
-
-
-
-if __name__ == "__main__":
-    main()
+pygame.init()
